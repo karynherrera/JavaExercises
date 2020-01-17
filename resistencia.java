21cc7a1e77c04845a86d9d9905163e9c4fbe7c12
karynHerrera <karynherrera@gmail.com>
2020-01-16 22:11:09 -0300

    actualizacion
    actualizacion

--- resistencia.java
+++ resistencia.java
@@ -9,27 +9,30 @@ import java.util.Scanner;
 public class Resistencia {
 
     public static void main(String[] args) {
-        int r1=0, r2=0, r3=0;
-        float rt;
-        Scanner sc = new Scanner(System.in);
+        int r1, r2, r3;
+        float sum, rt;
 
-        while (r1<= 0 || r2<= 0 ||r3<= 0) {
-            System.out.printf("Ingrese resistencia 1: \n");
-            r1 = sc.nextInt();
-            
-            System.out.printf("Ingrese resistencia 2: \n");
-            r2 = sc.nextInt();
-            
-            System.out.printf("Ingrese resistencia 3: \n");
-            r3 = sc.nextInt();
-           
-            if(r1<= 0 || r2<= 0 ||r3<= 0){
-                System.out.printf("Las resistencias no son distintas a cero.\n");
-            }
-            
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Ingrese resistencia 1: \n");
+        r1 = sc.nextInt();
+
+        System.out.printf("Ingrese resistencia 2: \n");
+        r2 = sc.nextInt();
+
+        System.out.printf("Ingrese resistencia 3: \n");
+        r3 = sc.nextInt();
+
+        if (r1 <= 0) {
+            System.out.printf("Las resistencias no son distintas a cero.\n");
+        } else if (r2 <= 0) {
+            System.out.printf("Las resistencias no son distintas a cero.\n");
+        } else if (r3 <= 0) {
+            System.out.printf("Las resistencias no son distintas a cero.\n");
+        } else {
+            rt = 1 / ((1 / r1) + (1 / r2) + (1 / r3));
+            System.out.printf("La resistencia total es de " + rt);
         }
-       rt= 1/((1/r1)+(1/r2)+(1/r3));
-       System.out.printf("La resistencia total es de "+rt);
+
     }
 
 }
